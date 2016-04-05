312e4feb2e24a5c89f3a982c980af79356b92b68
vizantiyec <vizantiyec@gmail.com>
2016-04-05 23:01:55 +0300

    refs add MONGODB

--- server.js
+++ server.js
@@ -1,5 +1,7 @@
 import express from 'express';
 
+import {MongoClient} from 'mongodb';
+
 let app = express();
 
 app.use(express.static('public'));
@@ -7,4 +9,11 @@ app.use(express.static('public'));
 
 app.listen(3000, () => console.log('Listening on port 3000'));
 
+MongoClient.connect(process.env.MONGO_URL, (err, database) => {
+    if(err) throw err;
 
+    database.collection('links').find({}).toArray((err, links) => {
+        if(err) throw err;
+        console.log(links);
+    });
+});
